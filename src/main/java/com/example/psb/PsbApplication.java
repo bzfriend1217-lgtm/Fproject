9706c3795ded1e8bd55ab1c6e2c6a2ad04a22ded
bzfriend1217-lgtm <bzfriend1217@gmail.com>
2026-07-01 13:52:45 +0900

    1

--- src/main/java/com/example/psb/PsbApplication.java
+++ src/main/java/com/example/psb/PsbApplication.java
@@ -7,6 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PsbApplication {
 
     public static void main(String[] args) {
+        System.out.println(PsbApplication.class);
+
+
         SpringApplication.run(PsbApplication.class, args);
     }
 }
